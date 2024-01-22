26f2334d6bce49a11414a42e8edac4cd3a3625d0
jrferguson <1q2w3e4r5t6y>
2024-01-22 09:31:52 +0000

    added transaction

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,10 @@
+import decimal
 from django.contrib import admin
 
-from .models import Member
+from .models import Member, Transaction
+from django.core.exceptions import ObjectDoesNotExist
+import csv
+from django.http import HttpResponse
 
 
 @admin.register(Member)
@@ -21,7 +25,13 @@ class MemberAdmin(admin.ModelAdmin):
         "membership_start",
         "origin",
     )
-    list_display = ("name", "middle_name", "surname", "member_type", "origin",)
+    list_display = (
+        "name",
+        "middle_name",
+        "surname",
+        "member_type",
+        "origin",
+    )
     list_filter = (
         "name",
         "surname",
@@ -31,3 +41,109 @@ class MemberAdmin(admin.ModelAdmin):
         "origin",
         "created_at",
     )
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    search_fields = ("type__startswith",)
+    fields = (
+        "amount",
+        "type",
+        "member",
+        "date",
+        "month",
+        "service_type",
+    )
+    list_display = ("type", "member_name", "date", "month")
+    list_filter = ("type", "date", "member", "month")
+    actions = ["export_to_csv", "export_to_xls"]
+
+    def member_name(
+        self, instance
+    ):  # name of the method should be same as the field given in `list_display`
+        try:
+            return f"{instance.member.name}  {instance.member.surname}"
+        except ObjectDoesNotExist:
+            return "ERROR!!"
+
+    @admin.action(description="Mark selected stories as published")
+    def export_to_csv(modeladmin, request, queryset):
+        print(queryset)
+        total_amount = decimal.Decimal(0.00)
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment;" "filename={}.csv".format(
+            "transaction_outline"
+        )
+        writer = csv.writer(response)
+        writer.writerow(
+            [
+                "Full Name",
+                "Type",
+                "Service type",
+                "Month",
+                "Date",
+                "Amount",
+            ]
+        )
+        for s in queryset:
+            total_amount = total_amount + s.amount
+            writer.writerow(
+                [
+                    f"{s.member.name} {s.member.middle_name} {s.member.surname}",
+                    s.type,
+                    s.service_type,
+                    s.month,
+                    s.date.strftime("%d/%m/%Y"),
+                    s.amount,
+                ]
+            )
+
+        writer.writerow(
+            [
+                "Total Amount","","","","",total_amount,
+            ]
+        )
+        return response
+
+    export_to_csv.short_description = "Export to CSV"  # short description
+
+    @admin.action(description="Mark selected stories as published")
+    def export_to_xls(modeladmin, request, queryset):
+        print(queryset)
+        total_amount = decimal.Decimal(0.00)
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment;" "filename={}.csv".format(
+            "transaction_outline"
+        )
+        writer = csv.writer(response)
+        writer.writerow(
+            [
+                "Full Name",
+                "Type",
+                "Service type",
+                "Month",
+                "Date",
+                "Amount",
+            ]
+        )
+        for s in queryset:
+            total_amount = total_amount + s.amount
+            writer.writerow(
+                [
+                    f"{s.member.name} {s.member.middle_name} {s.member.surname}",
+                    s.type,
+                    s.service_type,
+                    s.month,
+                    s.date.strftime("%d/%m/%Y"),
+                    s.amount,
+                ]
+            )
+
+        writer.writerow(
+            [
+                "Total Amount","","","","",total_amount,
+            ]
+        )
+        return response
+
+    export_to_xls.short_description = "Export to XLS"  # short description
--- members/models.py
+++ members/models.py
@@ -1,4 +1,7 @@
+import uuid
 from django.db import models
+
+from activities.models import MONTH
 
 DEPARTMENTS = (
     ("MEN", "Men's Ministry"),
@@ -24,20 +27,32 @@ ORIGIN = (
         ('MANUAL', 'Manual',),
     )
 
+TRANSACTION_TYPE = (
+        ('OFFERING', 'Offering',),
+        ('TITHE', 'Tithe',),
+        ('FREE DONATION', 'Free Donation'),
+    )
+
+SERVICE_TYPE = (
+        ('FRIDAY_PRAYER_MEETING', 'Friday Prayer Meeting',),
+        ('1_SUNDAY_SERVICE', 'Sunday 1st Service',),
+        ('2_SUNDAY_SERVICE', 'Sunday 2nd Service'),
+    )
+
 # Create your models here.
 class Member(models.Model):
     name = models.CharField("Name", max_length=255,blank=False )
     middle_name = models.CharField("Middle Name", max_length=255,blank=True)
     surname = models.CharField("Surname", max_length=255,blank=False)
-    telephone = models.CharField("Telephone", max_length=255,blank=False)
+    telephone = models.CharField("Telephone", max_length=255, blank=True, null=True)
     postcode = models.CharField("Postcode",max_length=255, blank=True, null=True)
     address = models.CharField("Address",max_length=255, blank=True, null=True)
     date_of_birth = models.CharField("Date Of Birth",max_length=255, blank=True, null=True)
     age = models.IntegerField("Age", blank=True, null=True)
-    sex = models.CharField(max_length=50,choices=SEX_CHOICES)
+    sex = models.CharField(max_length=50,choices=SEX_CHOICES, blank=True, null=True)
     department = models.CharField("Department",max_length=255,choices=DEPARTMENTS, blank=True, null=True)
-    member_type = models.CharField("Member type",max_length=255,choices=MEMBER_TYPE, default='Full Member')
-    origin = models.CharField("Origin",max_length=255,choices=ORIGIN, default='Manual')
+    member_type = models.CharField("Member type",max_length=255,choices=MEMBER_TYPE, default='Full Member',  blank=True, null=True)
+    origin = models.CharField("Origin",max_length=255,choices=ORIGIN, default='Manual',  blank=True, null=True)
     membership_start = models.DateField("Membership start", max_length=255, blank=True, null=True)
     created_at = models.DateTimeField("Created at", auto_now_add=True)
 
@@ -50,3 +65,29 @@ class Member(models.Model):
 
     def __str__(self):
         return f"{self.name} {self.middle_name} {self.surname}"
+
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField("Type",max_length=255,choices=TRANSACTION_TYPE)
+    member = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='members'
+    )
+    service_type = models.CharField("Service Type",max_length=255,choices=SERVICE_TYPE, blank=True, null=True)
+    date = models.DateField("Date",max_length=255)
+    month = models.CharField("Month", max_length=255, choices=MONTH, blank=False, null=False)
+    created_at = models.DateTimeField("Created at", auto_now_add=True)
+
+    class Meta:
+        ordering = ("type", "created_at")
+        verbose_name_plural = "Transactions"
+
+    def __unicode__(self):
+        return "%s: /n %s  %s" % (self.type, self.created_at)
+
+    def __str__(self):
+        return f"{self.type} - {self.member.name} {self.member.surname}"
+    
+    

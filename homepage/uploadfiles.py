56e0429f237ee2fbe8fade8b147985217a16eeb2
jrferguson <1q2w3e4r5t6y>
2024-08-14 17:44:04 +0100

    added image auto resize

--- homepage/uploadfiles.py
+++ homepage/uploadfiles.py
@@ -1,7 +1,5 @@
 import os
 import random
-from django.core.exceptions import ValidationError
-from django.core.files.images import get_image_dimensions
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -47,33 +45,3 @@ def homepage_leader_cover_upload_image_path(instance, filename):
     return "homepage/leader/{final_filename}".format(
         new_filename=new_filename, final_filename=final_filename
     )
-
-def homepage_banner_image_restriction(image):
-    image_width, image_height = get_image_dimensions(image)
-    if image_width > 203 or image_height > 223:
-        raise ValidationError('Image width needs to be less than height: 223 px width: 204 px')
-    
-def homepage_background_image_restriction(image):
-    image_width, image_height = get_image_dimensions(image)
-    if image_width > 1921 or image_height > 905:
-        raise ValidationError('Image width needs to be less than height: 905 px width: 1921 px')
-    
-def homepage_about_us_image_restriction(image):
-    image_width, image_height = get_image_dimensions(image)
-    if image_width > 371 or image_height > 421:
-        raise ValidationError('Image width needs to be less than height: 421 px width: 371 px')
-
-def about_us_image_restriction(image):
-    image_width, image_height = get_image_dimensions(image)
-    if image_width > 271 or image_height > 301:
-        raise ValidationError('Image width needs to be less than height: 301 px width: 271 px')
-
-def leader_image_restriction(image):
-    image_width, image_height = get_image_dimensions(image)
-    if image_width > 467 or image_height > 495:
-        raise ValidationError('Image width needs to be less than height: 495 px width: 467 px')
-
-def video_image_restriction(image):
-    image_width, image_height = get_image_dimensions(image)
-    if image_width > 561 or image_height > 430:
-        raise ValidationError('Image width needs to be less than height: 430 px width: 561 px')
